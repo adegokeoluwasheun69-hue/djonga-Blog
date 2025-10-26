9935f6c42e672f345518d42bee38211469f38882
sheun <adegokeoluwasheun69@gmail.com>
2025-10-27 00:49:20 +0100

    home delets

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
 from.models import Blog
-from .models import Service, Product
+
 
 
 # Register your models here.
 admin.site.register(Blog)
 
 
-admin.site.register(Service)
-admin.site.register(Product)
--- blog/models.py
+++ blog/models.py
@@ -11,20 +11,3 @@ class Blog(models.Model):
     
     def __str__(self):
         return f"{self.title}"
-
-class Service(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    icon = models.ImageField(upload_to='services/icons/')
-
-    def __str__(self):
-        return self.title
-
-class Product(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    image = models.ImageField(upload_to='products/images/')
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-
-    def __str__(self):
-        return self.name
